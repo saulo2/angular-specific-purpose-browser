2ed4f2e3af72666e77ddb9465198a189711294cf
Saulo Medeiros de Araujo <saulo2@gmail.com>
2016-04-11 19:26:53 -0300

    Deletion implemented

--- src/project/projectEdit.js
+++ src/project/projectEdit.js
@@ -1,7 +1,7 @@
 (function() {
     "use strict"
 
-    angular.module("timesheet").controller("projectEditing", function($location, $route, $scope, url) {
+    angular.module("timesheet").controller("projectEdit", function($location, $route, $scope, url) {
         $scope.save = function() {
             $scope.resource.$post("save", null, $scope.resource.project).then(function(redirect) {
                 $location.url(url(redirect.location))
